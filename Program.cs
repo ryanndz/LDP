f62eb3c96ca809ae727ddb1757bb1974fbb9a400
ryanndz <ryan.lopes2506@gmail.com>
2024-03-18 17:01:40 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿byte idade= 35; 
+Console.WriteLine(idade);
+string nome = ("maria");
+Console.WriteLine(nome);
+float altura= 3.45f;
+Console.WriteLine(altura);
+const int ano = 12; 
+Console.WriteLine(ano);
+double? nota = 7.80;
+Console.WriteLine(nota);
+//6) Variáveis de tipos de referência armazenam referências em seus dados
+// (objetos) enquanto que variáveis de tipos de valor contém diretamente seus dados.
+//7)Nullable Types é um recurso do C# que nos
+// permite atribuir o valor null a um tipo de dado que, por padrão, não aceita valores nulos.
+//8) Camel Case é uma forma de escrever palavras compostas onde cada palavra começa com letra maiúscula, 
+//exceto a primeira que começa com letra minúscula. Por exemplo: nomeDaVariavel, calcularIdadeDoUsuario, 
+//numeroDeTelefone.
+//9)Pascal Case é uma convenção de nomenclatura onde cada palavra começa com letra maiúscula, 
+//incluindo a primeira. Por exemplo: NomeDaVariavel, CalcularIdadeDoUsuario, NumeroDeTelefone.
+int x= 77;
+int y= 66;
+Console.WriteLine(x+y);
+//Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e string.
+//Bool = False 
+//int = 0
+//char: \0
+//double: 0.0
+//float: 0.0f
+//decimal: 0.0m
+//string: null
